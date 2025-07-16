93e8b55fc6b89ccbedbae18aff2e53354f33dfd7
Aram Grigoryan <grigoryanaram88@gmail.com>
2025-07-16 19:03:06 +0400

    add viper for the configuration reading

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 
 	"github.com/aram88/simplebank/api"
 	db "github.com/aram88/simplebank/db/sqlc"
+	"github.com/aram88/simplebank/util"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const (
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:7777"
-)
-
 func main() {
-	conn, err := pgxpool.New(context.Background(), dbSource)
+	config, err := util.LoadConfig(".s")
+	if err != nil {
+		log.Fatal("cannnot load configurations", err)
+	}
+
+	conn, err := pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -23,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
