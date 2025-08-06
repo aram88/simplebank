6527f83dcae35aa76e243cd6a44aa6a840c14ff5
Aram Grigoryan <grigoryanaram88@gmail.com>
2025-08-06 18:07:35 +0400

    structurel change to the server add config in the server

--- api/user.go
+++ api/user.go
@@ -7,6 +7,7 @@ import (
 	db "github.com/aram88/simplebank/db/sqlc"
 	"github.com/aram88/simplebank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
@@ -17,12 +18,22 @@ type createUserRequest struct {
 	Emial    string `json:"email"  binding:"required,email"`
 }
 
-type createUserRespons struct {
+type userResponse struct {
 	Username          string    `json:"username"`
 	FullName          string    `json:"full_name"`
 	Email             string    `json:"email"`
 	PasswordChangedAt time.Time `json:"password_changed_at"`
 	CreatedAt         time.Time `json:"created_at"`
+}
+
+func newUserResponce(user db.User) userResponse {
+	return userResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt.Time,
+		CreatedAt:         user.CreatedAt.Time,
+	}
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -54,12 +65,54 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
-	rsp := createUserRespons{
-		Username:          user.Username,
-		FullName:          user.FullName,
-		Email:             user.Email,
-		PasswordChangedAt: user.PasswordChangedAt.Time,
-		CreatedAt:         user.CreatedAt.Time,
+	rsp := newUserResponce(user)
+	ctx.JSON(http.StatusOK, rsp)
+}
+
+type loginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password"  binding:"required,min=6"`
+}
+
+type loginUserResponse struct {
+	AccseesToken string       `json:"access_token"`
+	User         userResponse `json:"user"`
+}
+
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponce(err))
+		return
 	}
+
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			ctx.JSON(http.StatusAccepted, errorResponce(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponce(err))
+		return
+	}
+
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponce(err))
+		return
+	}
+
+	accessToken, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponce(err))
+		return
+	}
+
+	rsp := loginUserResponse{
+		AccseesToken: accessToken,
+		User:         newUserResponce(user),
+	}
+
 	ctx.JSON(http.StatusOK, rsp)
 }
--- util/config.go
+++ util/config.go
@@ -1,14 +1,18 @@
 package util
 
 import (
+	"time"
+
 	"github.com/spf13/viper"
 )
 
 // Config stores all configwuration of the application.
 // The value are ready by viper from a config file or enviroment variables.
 type Config struct {
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 // LoadConfig reads configuration from file or enviroment variables.
